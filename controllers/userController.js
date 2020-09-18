34e041f7c134cac4d6922c18836faf5eb71de83a
dev-woong <dev-woong>
2020-09-18 23:29:51 +0900

    add : complete HTML

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,10 @@ export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" }
 export const postLogin = (req, res) => {
   res.redirect(routes.home)
 }
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" })
+export const logout = (req, res) => {
+  //To Do : Process Log Out
+  res.redirect(routes.home)
+}
 export const users = (req, res) => res.render("users", { pageTitle: "Users" })
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" })
 export const changePassword = (req, res) =>
