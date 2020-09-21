0a4f6c6e519fc9fb2e6a769ea1ab68a9cfb059e3
dev-woong <dev-woong>
2020-09-21 22:40:05 +0900

    add : init mongoDB

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,15 @@
-import { videos } from "../db"
 import routes from "../routes"
-export const home = (req, res) => res.render("home", { pageTitle: "Home", videos })
+import Video from "../models/Video"
+
+export const home = async(req, res) => {
+  try{
+ const videos = await Video.find({});
+  res.render("home", { pageTitle: "Home", videos })
+  }catch(error){
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos:[] })
+  }
+}
 export const search = (req, res) => {
   const {
     query: { term: searchingBy },
@@ -9,12 +18,19 @@ export const search = (req, res) => {
 }
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" })
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file:{path}
   } = req
+  const newVideo = await Video.create({
+    fileUrl : path,
+    title,
+    description
+  })
+  console.log(newVideo);
   // To Do : Upload and save Video
-  res.redirect(routes.videoDetail(32421))
+  res.redirect(routes.videoDetail(newVideo.id))
 }
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" })
--- db.js
+++ db.js
@@ -1,55 +1,16 @@
-export const videos = [
-  {
-    id: 32421,
-    title: "Video awesome",
-    description: "This is something",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "devWoong",
-      email: "dev_woong@naver.com",
-    },
-  },
-  {
-    id: 55555,
-    title: "Video Super",
-    description: "This is something",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "devWoong",
-      email: "dev_woong@naver.com",
-    },
-  },
-  {
-    id: 76666,
-    title: "Video Nice",
-    description: "This is something",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "devWoong",
-      email: "dev_woong@naver.com",
-    },
-  },
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
 
-  {
-    id: 32413,
-    title: "Video Perpect",
-    description: "This is something",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "devWoong",
-      email: "dev_woong@naver.com",
-    },
-  },
-]
+mongoose.connect(
+  process.env.MONGO_URL
+)
+
+const db = mongoose.connection
+
+const handleOpen = () => console.log("★ Connected to DB")
+const handleError = error => console.log(` ※ Error on DB Connection: ${error}`)
+
+
+db.once("open", handleOpen)
+db.on("error",handleError)
