da2addd06d50026fb17962e1c129596b3b2313d0
dev-woong <occhoi90@gmail.com>
2020-09-22 22:55:06 +0900

    Video Controller is Completed

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,7 +1,8 @@
 import express from "express"
 import {
   deleteVideo,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
   getUpload,
   videoDetail,
   postUpload,
@@ -11,11 +12,18 @@ import {uploadVideo} from "../middlewares"
 
 const videoRouter = express.Router()
 
-// videoRouter.get(routes.videos, videos)
+// Upload
 videoRouter.get(routes.upload, getUpload)
 videoRouter.post(routes.upload, uploadVideo,postUpload)
+
+//Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail)
-videoRouter.get(routes.editVideo, editVideo)
-videoRouter.get(routes.deleteVideo, deleteVideo)
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo)
+videoRouter.post(routes.editVideo(), postEditVideo)
+
+//Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo)
 
 export default videoRouter
